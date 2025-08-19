d1dbd78ddde00a0022a6dd3d7fbcdbf510f960d7
Vic-M-S <victoriasantiago450@gmail.com>
2025-08-19 18:24:59 -0300

    Initial commit

---
+++ js/tabuada.js
@@ -0,0 +1,35 @@
+let botoes = ''
+console.log(1)
+for (let i = 0; i <= 10; i++) {
+    let botao = `<button name="botao" class="btn btn-primary">${i}</button>`;
+    // console.log(i);
+    botoes = botoes + '&nbsp;' + botao;
+    // botoes+='&nbsp;${botao}';
+    // console.log(botoes);
+}
+
+document.getElementById("valores").innerHTML = botoes;
+
+const arrayBotao = document.getElementsByName("botao");
+console.log(typeof arrayBotao);
+console.log(arrayBotao);
+console.log(arrayBotao.length);
+console.log(arrayBotao[10].innerText);
+
+arrayBotao.forEach(function (botaoAtual) {
+    // console.log(botaoAtual.innerText);
+    botaoAtual.addEventListener("mouseover", function () {
+        // alert(`Olá ${botaoAtual.innerText}`);
+        let tab = botaoAtual.innerHTML;
+        let conjunto = "";
+        for (let i = 0; i <= 10; i++) {
+            conjunto += `${tab}x ${i} = ${tab * i}<br>`;
+        }
+        document.getElementById("resultado").innerHTML = conjunto;
+
+    });
+
+    botaoAtual.addEventListener("mouseleave", function () {
+        document.getElementById("resultado").innerHTML = "";
+    });
+});
